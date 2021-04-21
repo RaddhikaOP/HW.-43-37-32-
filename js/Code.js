fd069ad0a0cce5117e15d7f028baca248029677d
RaddhikaOP <75173351+RaddhikaOP@users.noreply.github.com>
2021-04-21 17:21:21 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,28 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+
+fill("white")
+textSize(15)
+text("C U T N I F O N", 640,150)
+fill("lightblue")
+text("Hint: A group of code which we can make on our own.", 640,170)
+
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 100,300)
+    fill("lightblue")
+    text("Hint: It starts with D and ends with E ", 100,320)
+    
+
+}
+
